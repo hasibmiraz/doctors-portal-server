be960b588a817fd74a397eab24c63a569a140494
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-12 01:56:27 +0600

    Added get method for all services

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -7,6 +8,30 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.02ql6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollection = client
+      .db('doctors_portal')
+      .collection('services');
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
