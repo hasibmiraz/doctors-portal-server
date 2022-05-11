01be969eeccfd67afd51f4600087ea2c3680e17a
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-12 01:24:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
