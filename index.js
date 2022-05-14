28703d49d7382f0e9211f5d76456a9d2ba371921
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-14 16:39:48 +0600

    Added post method for booking

--- index.js
+++ index.js
@@ -22,11 +22,27 @@ async function run() {
     const servicesCollection = client
       .db('doctors_portal')
       .collection('services');
+
+    const bookingCollection = client
+      .db('doctors_portal')
+      .collection('bookings');
+
     app.get('/service', async (req, res) => {
       const query = {};
       const cursor = servicesCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patientEmail: booking.patientEmail,
+      };
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     });
   } finally {
   }
