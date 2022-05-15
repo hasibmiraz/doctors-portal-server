bb78e4740f7c94d6ae2c25bb9eebc80d17f90bd1
hasibmiraz <hasibmiraz09@gmail.com>
2022-05-16 04:15:14 +0600

    User route set according to role

--- index.js
+++ index.js
@@ -57,12 +57,27 @@ async function run() {
       res.send(users);
     });
 
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin });
+    });
+
     app.put('/user/admin/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
-      const filter = { email };
-      const updateDoc = { $set: { role: 'admin' } };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+
+      if (requesterAccount.role === 'admin') {
+        const filter = { email };
+        const updateDoc = { $set: { role: 'admin' } };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        return res.send(result);
+      }
+      return res.status(403).send({ message: 'Forbidden access!' });
     });
 
     app.put('/user/:email', async (req, res) => {
